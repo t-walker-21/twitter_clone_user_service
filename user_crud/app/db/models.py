6aff166f1d47afbca4de82ec8739a1162bf0d02b
Walker, Tevon <TEVON_WALKER@homedepot.com>
2024-12-03 15:21:58 -0500

    Add auth protection on endpoints, add follower feature

--- user_crud/app/db/models.py
+++ user_crud/app/db/models.py
@@ -9,3 +9,8 @@ class User(SQLModel, table=True):
     email: str = Field(index=True, unique=True)      # Unique email
     date_of_birth: date
     password: str
+
+class Follower(SQLModel, table=True):
+    id: int = Field(default=None, primary_key=True)  # Primary key
+    follower_id: int = Field(foreign_key="user.id")  # References User
+    followee_id: int = Field(foreign_key="user.id")   # References User
--- user_crud/app/main.py
+++ user_crud/app/main.py
@@ -1,13 +1,24 @@
 # app/main.py
 
 from fastapi import FastAPI, Depends, HTTPException
+from fastapi.security import OAuth2PasswordBearer
 from sqlmodel import Session, select
 from db.db_connection import get_session
-from db.models import User
+from db.models import User, Follower
 from datetime import datetime
 import jwt
 
 app = FastAPI()
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+def get_current_user(token: str = Depends(oauth2_scheme)):
+    
+    if token is None:
+        raise HTTPException(status_code=403, detail="Not authorized")
+    
+    user_info = jwt.decode(jwt=token, key="secret", algorithms=["HS256",])
+    return user_info  # In a real application, you would return a user object
 
 @app.post("/users/", status_code=201)
 def create_user(user: User, session: Session = Depends(get_session)):
@@ -26,7 +37,7 @@ def create_user(user: User, session: Session = Depends(get_session)):
     return {'success': True}
 
 @app.get("/users/")
-def get_users(session: Session = Depends(get_session)):
+def get_users(session: Session = Depends(get_session), current_user: str = Depends(get_current_user)):
 
     statement = select(User)
     results = session.exec(statement)
@@ -43,7 +54,7 @@ def get_users(session: Session = Depends(get_session)):
 
 
 @app.delete("/users/{user_id}")
-def get_users(user_id: int, session: Session = Depends(get_session)):
+def get_users(user_id: int, session: Session = Depends(get_session), current_user: str = Depends(get_current_user)):
 
     statement = select(User).where(User.id == user_id)
     results = session.exec(statement)
@@ -69,3 +80,40 @@ def get_users(email_address: str, password:str, session: Session = Depends(get_s
     token = jwt.encode(payload=payload_data, key='secret')
 
     return {'jwt_token': token}
+
+@app.post("/follows/", status_code=201)
+def follow_user(followee_id: int, session: Session = Depends(get_session), current_user: str = Depends(get_current_user)):
+
+    if str(followee_id) == current_user['sub']:
+        raise HTTPException(status_code=400, detail="User cannot follow itself")
+
+    existing_user = session.query(User).filter((User.id == followee_id)).first()
+
+    if not existing_user:
+        raise HTTPException(status_code=400, detail="User doesn't exist")
+    
+    existing_follow = session.query(Follower).filter((Follower.followee_id == followee_id) & (Follower.follower_id == current_user['sub'])).first()
+    if existing_follow:
+        raise HTTPException(status_code=400, detail=f"User {current_user['sub']} is already following {followee_id}")
+
+
+    link = Follower(follower_id=current_user['sub'], followee_id=followee_id)
+
+    session.add(link)
+    session.commit()
+    session.refresh(link)
+
+    return {'success': True}
+
+@app.get("/follows/")
+def get_followers(session: Session = Depends(get_session), current_user: str = Depends(get_current_user)):
+
+    statement = select(Follower).where(Follower.followee_id == current_user['sub'])
+    results = session.exec(statement)
+    response = []
+    for result in results:
+        response.append({'id': result.id,
+                         'follower_id': result.follower_id,
+                         'followee_id': result.followee_id})
+        
+    return {'users': response}
