7cedf8d1c51c1fb31fa0e13f9159207e6999c348
tevon walker <tevjwalker@gmail.com>
2025-06-09 18:36:08 -0400

    Add bio and profile pic field to user model

--- user_crud/app/db/models.py
+++ user_crud/app/db/models.py
@@ -1,16 +1,19 @@
 from sqlmodel import SQLModel, Field
 from datetime import date
 
-class User(SQLModel, table=True):
-    id: int = Field(default=None, primary_key=True)  # Primary key
+class Users(SQLModel, table=True):
+
+    id: int = Field(default=None, primary_key=True)  # Primary key that auto-increments
     username: str = Field(index=True, unique=True)   # Unique username
     first_name: str
     last_name: str
     email: str = Field(index=True, unique=True)      # Unique email
     date_of_birth: date
     password: str
+    profile_picture_url: str = Field(default=None, nullable=True)  # Optional profile picture URL
+    bio: str = Field(default=None, nullable=True)  # Optional bio field
 
 class Follower(SQLModel, table=True):
     id: int = Field(default=None, primary_key=True)  # Primary key
-    follower_id: int = Field(foreign_key="user.id")  # References User
-    followee_id: int = Field(foreign_key="user.id")   # References User
+    follower_id: int = Field(foreign_key="users.id")  # References User
+    followee_id: int = Field(foreign_key="users.id")   # References User
