b90237b14b958275981734a23a4491ce6465a37c
tevon walker <tevjwalker@gmail.com>
2025-06-09 18:35:33 -0400

    Import followers table

--- user_crud/app/db/db_connection.py
+++ user_crud/app/db/db_connection.py
@@ -1,8 +1,12 @@
 import os
 from sqlmodel import SQLModel, create_engine, Session
-from .models import User
+from .models import Users, Follower
 
-SQL_URL_STRING = os.environ.get("SQL_URL_STRING", "sqlite:///./test.db")
+SQL_URL_STRING = os.environ.get("SQL_URL_STRING")
+if SQL_URL_STRING is None:
+    raise ValueError("SQL_URL_STRING environment variable not set")
+else:
+    print(f"SQL_URL_STRING: {SQL_URL_STRING}")
 engine = create_engine(SQL_URL_STRING)
 SQLModel.metadata.create_all(engine)
 
